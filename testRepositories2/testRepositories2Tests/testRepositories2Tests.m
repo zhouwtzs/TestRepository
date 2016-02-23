e5f662e8d97c05794ec05c0a60e9963944a83b06
zhowtzs <2192115519@qq.com>
2016-02-23 16:05:42 +0800

    create test2

---
+++ testRepositories2/testRepositories2Tests/testRepositories2Tests.m
@@ -0,0 +1,39 @@
+//
+//  testRepositories2Tests.m
+//  testRepositories2Tests
+//
+//  Created by 周文涛 on 16/2/23.
+//  Copyright © 2016年 周文涛. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testRepositories2Tests : XCTestCase
+
+@end
+
+@implementation testRepositories2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
